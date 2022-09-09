bb417d584c2b52bd1df2d902cc67376662637bec
Sheila Arenillas <sheila.arenillas@bosonit.com>
2022-09-09 08:27:50 +0200

    seccion 17

--- Angular/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ Angular/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  /*miFormulario: FormGroup= new FormGroup ({
+    nombre: new FormControl('RTX  '),
+    precio: new FormControl(1500),
+    existencias: new FormControl(5)
+  })*/
 
-  ngOnInit(): void {
+  miFormulario: FormGroup= this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3) ]],
+    precio: [0 , [Validators.required, Validators.min(0) ] ],
+    existencias: [0, [Validators.required, Validators.min(0) ] ],
+  })
+
+  constructor( private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.miFormulario.reset({
+      nombre: 'c',
+      precio: 290,
+      existencias: 10
+    })
+    
   }
+
+ campoEsValido(campo:string){
+  return this.miFormulario.controls[campo].errors 
+  && this.miFormulario.controls[campo].touched
+
+ }
+
+ guardar(){
+  if(this.miFormulario.invalid){
+    this.miFormulario.markAllAsTouched();
+    return;
+  }
+
+  console.log(this.miFormulario.value)
+
+  this.miFormulario.reset();
+ }
 
 }
--- Angular/06-formulariosApp/src/app/reactive/reactive.module.ts
+++ Angular/06-formulariosApp/src/app/reactive/reactive.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
@@ -15,6 +17,7 @@ import { SwitchesComponent } from './switches/switches.component';
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     ReactiveRoutingModule
   ],
   exports:[
